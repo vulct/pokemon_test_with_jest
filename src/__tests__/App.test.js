a23e4a872fb9d2d7603378cb88b52495f52fb921
Tuan Vu Le Cong <tuanvu237362@gmail.com>
2022-05-31 17:49:18 +0700

    add test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -7,13 +7,28 @@ beforeEach(() => {
   wrapper = shallow(<App />);
 });
 
+describe("<App />", () => {
+  it("renders one <PokemonCollection /> component", () => {
+    expect(wrapper.find("PokemonCollection")).toHaveLength(1);
+  });
+
+  it("renders header App component", () => {
+    const header = <div className="pokemon-header">Pokemon</div>;
+    expect(wrapper.contains(header)).toBe(true);
+  });
+
+  describe("show button", () => {
+    it("renders button", () => {
+      expect(wrapper.find(".btn")).toHaveLength(1);
+    });
+
+    it("calls onClick callback", () => {
+      wrapper.find(".loadmore").simulate("click");
+      expect(wrapper.find(".loadmore").text()).toBe("Loading");
+    });
+  });
+});
+
 test("snapshot App component", () => {
   expect(wrapper).toMatchSnapshot();
 });
-
-test("render loading when clicked load more", () => {
-  wrapper.find(".loadmore").simulate("click");
-  expect(wrapper.find(".loadmore").text()).toBe("Loading");
-});
-
-
--- src/__tests__/PokemonCollection.test.js
+++ src/__tests__/PokemonCollection.test.js
@@ -1,11 +1,14 @@
 import { shallow } from "enzyme";
 import PokemonCollection from "../components/PokemonCollection";
+import PokemonList from "../components/PokemonList";
+
 
 describe("PokemonCollection", () => {
   let wrapper;
+  let testItems;
 
   beforeEach(() => {
-    const testItems = [
+    testItems = [
       {
         id: 1,
         name: "bob",
@@ -26,11 +29,16 @@ describe("PokemonCollection", () => {
   });
 
   it("Render <PokemonCollection>", () => {
-      expect(wrapper).toMatchSnapshot();
+    expect(wrapper).toMatchSnapshot();
   });
 
   it("length of collection should", () => {
     expect(wrapper.find(".collection-container")).toHaveLength(1);
   });
-  
+
+  it("test props", () => {
+    // expect(wrapper.props()).toEqual(1);
+    const header = shallow(<PokemonList name={'123'} image={'image'} />);
+    expect(header.find(".pokemon-name").text()).toBe("123");
+  })
 });
